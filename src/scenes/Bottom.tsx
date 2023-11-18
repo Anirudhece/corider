30531171fb617e3951d1a08999de40504e080b47
Anirudh <anirudhece2020@gmail.com>
2023-11-18 14:25:20 +0530

    ui done

--- src/scenes/Bottom.tsx
+++ src/scenes/Bottom.tsx
@@ -7,10 +7,18 @@ import {
   Button,
   InputGroup,
   InputRightElement,
+  Menu,
+  MenuButton,
+  IconButton,
+  MenuList,
+  MenuItem,
 } from "@chakra-ui/react";
 import { AiOutlineSend } from "react-icons/ai";
 import { GrAttachment } from "react-icons/gr";
 
+import { MdOutlineCameraAlt } from "react-icons/md";
+import { CiVideoOn } from "react-icons/ci";
+import { IoDocumentAttachOutline } from "react-icons/io5";
 const Bottom = () => {
   const [inputMessage, setInputMessage] = useState<string>("");
 
@@ -33,21 +41,41 @@ const Bottom = () => {
               value={inputMessage}
               onChange={(e) => setInputMessage(e.target.value)}
             />
-
-            <InputRightElement>
-              <Flex>
-                <Button variant="none">
-                  <GrAttachment />
-                </Button>
-                <Button
+            <Flex>
+              {/* <Button variant="none">
+                <GrAttachment />
+              </Button> */}
+              <Menu>
+                <MenuButton
+                  as={IconButton}
+                  aria-label="Options"
+                  icon={<GrAttachment />}
                   variant="none"
-                  isDisabled={inputMessage.trim().length === 0}
-                  onClick={handleSendMessage}
-                >
-                  <AiOutlineSend />
-                </Button>
-              </Flex>
-            </InputRightElement>
+                />
+                <MenuList>
+                  <Flex>
+                    <MenuItem
+                      icon={<MdOutlineCameraAlt />}
+                      width="auto"
+                      flex="1"
+                    />
+                    <MenuItem icon={<CiVideoOn />} width="auto" flex="1" />
+                    <MenuItem
+                      icon={<IoDocumentAttachOutline />}
+                      width="auto"
+                      flex="1"
+                    />
+                  </Flex>
+                </MenuList>
+              </Menu>
+              <Button
+                variant="none"
+                isDisabled={inputMessage.trim().length === 0}
+                onClick={handleSendMessage}
+              >
+                <AiOutlineSend />
+              </Button>
+            </Flex>
           </InputGroup>
         </Flex>
       </Box>
